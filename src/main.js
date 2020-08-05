07de0d1dae035f1d3551c183d827e65123cbb282
yifei520 <2318426929@qq.com>
2020-08-05 10:07:03 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import attachFastClick from "fastclick" // 引入fastclick解决移动端300ms延迟问题
 
-Vue.config.productionTip = false
+
+attachFastClick.attach(document.body) // 应用把该插件作用于整个body上
+Vue.config.productionTip = false // 阻止生产日志信息
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
